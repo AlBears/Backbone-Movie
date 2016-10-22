e726527c06ed342d91f9c8ef1cbdfc1c7d32e124
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-10-22 11:45:44 +0300

    Api search, parse

--- js/collections/MoviesCollection.js
+++ js/collections/MoviesCollection.js
@@ -1,4 +1,16 @@
-var Movies = Backbone.Collection.extend({
+MovieApp.Collections.Movies = Backbone.Collection.extend({
 
-        initialize: function(options) {}
+    initialize: function(options) {
+        if (options.title)
+            this.title = options.title;
+    },
+
+    url: function(){
+      return "http://www.omdbapi.com/?s="+ this.title;
+    },
+
+    parse: function(response){
+      return response.Search;
+    }
+
 });
--- js/views/SearchView.js
+++ js/views/SearchView.js
@@ -1,10 +1,24 @@
 MovieApp.Views.Search = Backbone.View.extend({
 
+    template: "<input type='text' placeholder='search'> \
+               <button>Search movie</button> \
+               <ul id='movie-list'></ul>",
+    events: {
+      'click button' : 'getmovies'
+    },
+
     initialize: function(options) {},
 
     render: function()
     {
-        this.$el.html("This is my search page!!");
+        this.$el.html(this.template);
         return this;
+    },
+
+    getmovies: function(){
+      var title = this.$el.find('input').val();
+      var movies = new MovieApp.Collections.Movies({title: title});
+
+      movies.fetch();
     }
 });
