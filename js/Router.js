32ceb09404d7c1a1aee2607853a68ddb8d19cd19
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-10-22 10:52:53 +0300

    Initial commit

---
+++ js/Router.js
@@ -0,0 +1,12 @@
+MovieApp.Router = Backbone.Router.extend({
+
+    routes: {
+        'home': 'home',
+        '*path': 'home'
+    },
+
+    home: function(){
+      var view = new MovieApp.Views.Home();
+      $('#main').html(view.render().el);
+    }
+});
