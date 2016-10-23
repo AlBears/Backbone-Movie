bbaee27c2517011f676f46fd6aa7f1b33a1d71ca
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-10-23 15:49:44 +0300

    improving some stuff

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Taken from http://blog.cloudoki.com/backbone-app-end-to-end-connecting-to-an-external-api/
+Initial code taken from http://blog.cloudoki.com/backbone-app-end-to-end-connecting-to-an-external-api/,
+new features added.
--- js/views/RegisterView.js
+++ js/views/RegisterView.js
@@ -1,67 +1,52 @@
 MovieApp.Views.Register = Backbone.View.extend({
 
   template: _.template(`
-<form id="registerForm">
-  <div class="form-group">
-      <label for="name" class="col-lg-2 control-label">Name</label>
-      <div class="col-lg-10">
-          <input type="text" class="form-control contact-name" id="name" name="name" />
-          <span class="help-block hidden"></span>
+    <form id="registerForm">
+      <div class="form-group">
+          <label for="name" class="col-lg-2 control-label">Name</label>
+          <div class="col-lg-10">
+              <input type="text" class="form-control contact-name" id="name" name="name" />
+              <span class="help-block hidden"></span>
+          </div>
       </div>
-  </div>
-  <div class="form-group">
-      <label for="email" class="col-lg-2 control-label">Email</label>
-      <div class="col-lg-10">
-          <input type="email" class="form-control contact-email" id="email" name="email" />
-          <span class="help-block hidden"></span>
+      <div class="form-group">
+          <label for="email" class="col-lg-2 control-label">Email</label>
+          <div class="col-lg-10">
+              <input type="email" class="form-control contact-email" id="email" name="email" />
+              <span class="help-block hidden"></span>
+          </div>
       </div>
-  </div>
-  <div class="form-group">
-      <div class="col-lg-offset-2 col-lg-10">
-          <button type="button" id="register" class="btn btn-success">Register</button>
+      <div class="form-group">
+          <div class="col-lg-offset-2 col-lg-10">
+              <button type="button" id="register" class="btn btn-success">Register</button>
+          </div>
       </div>
-  </div>
-</form>`),
+    </form>
+    `),
 
 
   events: {
-    'click #register': 'reg'
-  },
-
-    initialize: function() {
-      Backbone.Validation.bind(this);
+        'click #register': 'reg'
     },
 
-    render: function()
-    {
+    initialize: function() {
+        Backbone.Validation.bind(this);
+    },
+
+    render: function() {
         this.$el.html(this.template());
         return this;
     },
 
-    reg: function(e){
-      e.preventDefault();
-      var data = {
-        name:this.$('.contact-name').val(),
-        email:this.$('.contact-email').val()
-      }
-      //this.signUp(data);
-      this.model.set(data);
-      if(this.model.isValid(true)){
-      this.trigger('form:submitted', data);
-      // this.trigger('form:submitted', {
-      //   name: name,
-      //   email: email
-      // });
-    }
+    reg: function(e) {
+        e.preventDefault();
+        var data = {
+            name: this.$('.contact-name').val(),
+            email: this.$('.contact-email').val()
+        }
+        this.model.set(data);
+        if (this.model.isValid(true)) {
+            this.trigger('form:submitted', data);
+        }
     },
-
-    // signUp: function (data) {
-    //
-    //
-    //
-    //     if(this.model.isValid(true)){
-    //         // this.model.save();
-    //         alert('Great Success!');
-    //     }
-    // },
 });
